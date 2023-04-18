1a3ba7388405977ed4e8713d18b565284515d37d
Dheeraj batra <dheerajbatra75769@gmail.com>
2023-04-19 00:29:00 +0530

    fetch News using NewsApi

--- src/components/News.js
+++ src/components/News.js
@@ -2,11 +2,36 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      articles:[],
+      loading: false
+    }
+  }
+
+  async componentDidMount(){
+    let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=f7a668efdaee464a8252eb6fa1209adc";
+    let data=await fetch(url);
+    // JSON.parse() to convert the string into a JavaScript object:
+    let parsedData=await data.json();
+    this.setState({articles:parsedData.articles})
+  }
+
   render() {
     return (
-      <div>
-        This is a news component
-        <NewsItem/>
+      <div className="container my-3">
+        <h2>NewsMonkey-Top Headlines</h2>
+        <div className="row">
+          {this.state.articles.map((element) => {
+            return <div className="col-md-4" key={element.url} >
+              <NewsItem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageurl={element.urlToImage} newsurl={
+                element.url} />
+            </div>
+          })}; 
+
+        </div>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title,description,imageurl,newsurl}=this.props;
     return (
-      <div>
-        This is a news item
+      <div className='my-3'>
+        <div className="card" style={{width: "18rem"}}>
+          <img src={imageurl} className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{description}</p>
+              <a href={newsurl} target='_blank' className="btn btn-sm btn-primary">Read more</a>
+            </div>
+        </div>
       </div>
     )
   }
